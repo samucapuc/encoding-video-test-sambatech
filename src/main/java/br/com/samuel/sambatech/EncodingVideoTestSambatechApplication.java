d55b1a9af395898fa11505540f0973a9ad790a56
samucapuc <samucapuc@gmail.com>
2019-08-20 21:53:14 -0300

    Criação do projeto

---
+++ src/main/java/br/com/samuel/sambatech/EncodingVideoTestSambatechApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.sambatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodingVideoTestSambatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncodingVideoTestSambatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/samuel/sambatech/EncodingVideoTestSambatechApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.samuel.sambatech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class EncodingVideoTestSambatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
