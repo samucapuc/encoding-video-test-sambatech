b103c691b7a10088ba840690c1f48fdc0325d97d
samucapuc <samucapuc@gmail.com>
2019-08-24 10:31:42 -0300

    Configurando mensagens i18n

---
+++ src/main/java/br/com/samuel/sambatech/utils/MessageUtils.java
@@ -0,0 +1,23 @@
+package br.com.samuel.sambatech.utils;
+
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import lombok.NoArgsConstructor;
+
+@Component
+@NoArgsConstructor
+public class MessageUtils {
+
+  @Autowired
+  private MessageSource message;
+
+  public String getMessage(String key) {
+    return message.getMessage(key, null, Locale.US);
+  }
+
+  public String getMessage(String key, Object[] args) {
+    return message.getMessage(key, args, Locale.US);
+  }
+}
