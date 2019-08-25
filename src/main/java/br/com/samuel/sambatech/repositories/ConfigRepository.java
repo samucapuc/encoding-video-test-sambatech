cf5af00cc9e77f7cbd0500a9888e2cdc40106842
samucapuc <samucapuc@gmail.com>
2019-08-25 11:46:08 -0300

    Correção e implementação do service e repositorio do config

--- src/main/java/br/com/samuel/sambatech/repositories/ConfigRepository.java
+++ src/main/java/br/com/samuel/sambatech/repositories/ConfigRepository.java
@@ -1,8 +1,10 @@
 package br.com.samuel.sambatech.repositories;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import br.com.samuel.sambatech.domain.Config;
 
 @Repository
-public interface ConfigRepository {
+public interface ConfigRepository extends MongoRepository<Config, String> {
 
 }
--- src/main/java/br/com/samuel/sambatech/services/ConfigService.java
+++ src/main/java/br/com/samuel/sambatech/services/ConfigService.java
@@ -1,8 +1,21 @@
 package br.com.samuel.sambatech.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import br.com.samuel.sambatech.domain.Config;
+import br.com.samuel.sambatech.repositories.ConfigRepository;
 
 @Service
 public class ConfigService {
 
+  @Autowired
+  private ConfigRepository configRepository;
+
+  public Config findFirst() {
+    return configRepository.findAll().stream().findFirst().orElse(null);
+  }
+
+  public void save(Config cf) {
+    configRepository.save(cf);
+  }
 }
