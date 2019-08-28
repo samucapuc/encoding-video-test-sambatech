ed38bb978c7f32e246f2342d2fbfd1ee1ea78a76
samucapuc <samucapuc@gmail.com>
2019-08-28 18:25:44 -0300

    Teste Unitário criando output s3

--- src/test/java/br/com/samuel/sambatech/EncodingVideoTestSambatechApplicationTests.java
+++ src/test/java/br/com/samuel/sambatech/EncodingVideoTestSambatechApplicationTests.java
@@ -1,14 +1,36 @@
 package br.com.samuel.sambatech;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import br.com.samuel.sambatech.domain.Config;
+import br.com.samuel.sambatech.dto.outputs.OutputS3DTO;
+import br.com.samuel.sambatech.services.MainService;
+import br.com.samuel.sambatech.services.VideoEncodingService;
 
 @RunWith(MockitoJUnitRunner.class)
-class EncodingVideoTestSambatechApplicationTests {
+public class EncodingVideoTestSambatechApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @InjectMocks
+  private VideoEncodingService videoEncodingService;
+
+  @Mock
+  private MainService mainService;
+
+
+  @Test
+  public void createOutputS3() {
+    OutputS3DTO s3 = new OutputS3DTO("1");
+    Config config = new Config();
+    Mockito
+        .when(mainService.returnObject(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
+        .thenReturn(s3);
+    videoEncodingService.createOutputS3(config);
+    assertEquals(config.getIdOutputS3(), "1");
+  }
 
 }
