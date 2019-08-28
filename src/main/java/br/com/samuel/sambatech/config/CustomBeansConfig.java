207fe56773150cf0ce5fd4b07b363bc5520bd587
samucapuc <samucapuc@gmail.com>
2019-08-28 00:01:56 -0300

    Refatoração

--- src/main/java/br/com/samuel/sambatech/config/CustomBeansConfig.java
+++ src/main/java/br/com/samuel/sambatech/config/CustomBeansConfig.java
@@ -3,12 +3,18 @@ package br.com.samuel.sambatech.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class CustomBeansConfig {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 }
